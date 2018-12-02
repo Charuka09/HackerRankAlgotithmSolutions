44d5f49582b2251f7260fd01f271af65c2453aa5
charuka herath <charuka09@gmail.com>
2018-12-02 10:07:51 +0530

    initial commit

---
+++ WarmUp/SolveMeFirst.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    int sum = num1 + num2; 
+    //sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
